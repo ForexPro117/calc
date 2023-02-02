12241ca2197c18dca365211c9190dfeef9aeab38
ForexPro <roma.minihanowa@mail.ru>
2023-02-02 23:49:16 +0400

    init

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val editText:EditText = findViewById(R.id.editText)
+        intent.putExtra("message",editText.text)
+    }
+}
